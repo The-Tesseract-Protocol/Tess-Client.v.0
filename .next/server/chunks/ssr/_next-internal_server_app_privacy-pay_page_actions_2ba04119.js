326a0e7722c37a138e436b59d2cc88dd784ff09f
Dead-Bytes <143434285+Dead-Bytes@users.noreply.github.com>
2026-02-01 16:35:56 +0530

    Delete .next directory

--- .next/server/chunks/ssr/_next-internal_server_app_privacy-pay_page_actions_2ba04119.js
+++
@@ -1,3 +0,0 @@
-module.exports=[61947,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_privacy-pay_page_actions_2ba04119.js.map
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"OMDE7IUPxlwqN/Zaxanzle4Q4gYroPqpECreloUc1nA=\"\n}"
